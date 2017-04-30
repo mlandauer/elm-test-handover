9d0c497bde85a898a351a3225573d435818f285c
Matthew Landauer <mlandauer@gmail.com>
2017-04-30 12:04:23 +1000

    White flash is super visible with a large number of items in the view

--- Main.elm
+++ Main.elm
@@ -6,20 +6,7 @@ import Arithmetic
 
 
 main =
-    let
-        n =
-            100000
-    in
-        div [ class "box green" ]
-            [ text
-                ("Sum of primes below "
-                    ++ (toString n)
-                    ++ ": "
-                    ++ (toString (sumOfPrimesBelow n))
-                )
-            ]
-
-
-sumOfPrimesBelow n =
-    Arithmetic.primesBelow n
-        |> List.sum
+    div [ class "box green" ]
+        [ ul []
+            (List.map (\n -> li [] [ text (toString n) ]) (List.range 1 10000))
+        ]
